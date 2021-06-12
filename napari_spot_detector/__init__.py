14fce0dbf6a9040d50d4d1a4d6612b52545bda83
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-06-12 18:07:00 +0200

    initial commit

---
+++ napari_spot_detector/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.0.1"
+
+
+from ._reader import napari_get_reader
+
+
+from ._function import napari_experimental_provide_function
---
+++ napari_spot_detector/_tests/test_function.py
@@ -0,0 +1,7 @@
+# from napari_spot_detector import threshold, image_arithmetic
+
+# add your tests here...
+
+
+def test_something():
+    pass
